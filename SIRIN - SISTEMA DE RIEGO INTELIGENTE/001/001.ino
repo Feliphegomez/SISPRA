f6079cf468c47d55f61789a396260c17fdfd392e
Andres Felipe Gomez Maya <andres.gomez@MONTEVERDE.LOCAL>
2019-11-29 16:08:29 -0500

    Update.

--- SIRIN - SISTEMA DE RIEGO INTELIGENTE/001/001.ino
+++ SIRIN - SISTEMA DE RIEGO INTELIGENTE/001/001.ino
@@ -2,17 +2,20 @@
 #include <Wire.h>
 #include <SimpleDHT.h>
 
-int pinFC = A0;             // LED  - PIN 8 para LED
+int pinFC = A0;             // 
+int pinEV = 9;              // Electrovalvula - PIN 9
 int pinTX = 6;              // BT1  - PIN 6 como TX
 int pinRX = 5;              // BT1  - PIN 5 como RX
 int pinForceAT = 4;         // BT1  - PIN 4 para forzar modo AT de configuración
 int pinVCC_BT = 3;          // BT1  - PIN 3 como alimentacion 3.3V para modulo BT
 int pinDTH = 2;             // DHT  - PIN 2 para DTH
-int pinMoisture = A0;       // DHT  - PIN 2 para DTH
-int pinWaterSensor = A1;    // DHT  - PIN 2 para DTH
-int pinDHT22 = pinDTH;
+int pinMoisture = A0;       // 
+int pinWaterSensor = A1;    // 
 
-SimpleDHT22 dht22(pinDHT22);
+int statusEV = 0;
+volatile byte relayEVState = LOW;
+
+SimpleDHT22 dht22(pinDTH);
 SoftwareSerial BT1(pinRX, pinTX);  // Crear BT1 - pin 10 como RX, pin 11 como TX
 
 void setup() {
@@ -34,6 +37,10 @@ void setup() {
   if(digitalRead(pinForceAT) == HIGH){
     Serial.println("Modo configuracion de AT Bluetooth:");
   }
+  
+  pinMode(pinEV, OUTPUT);         // Electrovalvula
+  digitalWrite(pinEV, HIGH);
+  // attachInterrupt(digitalPinToInterrupt(PIRInterrupt), detectMotion, RISING);
 }
 
 void loop() {
@@ -65,30 +72,47 @@ void loop() {
       delay(1000);
     return;
   } else {
-      delay(631);
-      Serial.print("OK: ");
-      // Serial.print((float)temperature); Serial.print(" *C, ");
-      // Serial.print((float)humidity); Serial.println(" RH%");
-      //Send data over Bluetooth
-      // Serial.print("*T"+String(temperature)+"*");
-      // Serial.print("*H"+String(humidity)+"*");
+      //delay(631);
+      Serial.println("DHT: ");
+     // Serial.print((float)temperature); Serial.print(" *C, ");
+      //Serial.print((float)humidity); Serial.println(" RH%");
+      Serial.println("*T"+String(temperature)+"*");
+      Serial.println("*H"+String(humidity)+"*");
       digitalWrite(LED_BUILTIN, HIGH);
       delay(1000);
       
+      //Send data over Bluetooth
       BT1.println("*T"+String(temperature)+"*");
       BT1.println("*H"+String(humidity)+"*");
       digitalWrite(LED_BUILTIN, LOW);
       delay(1000);
-      
+
       int sensorWater =analogRead(pinWaterSensor);
+      Serial.println("SensorWater: ");
+      Serial.println("*W"+String(sensorWater)+"*");
       BT1.println("*W"+String(sensorWater)+"*");
       digitalWrite(LED_BUILTIN, HIGH);
       delay(1000);
-      
+
       int sensorMoisture =analogRead(pinMoisture);
-      sensorMoisture = map(sensorMoisture,550,0,0,100);
+      sensorMoisture = map(sensorMoisture,1023,0,0,100);
+      Serial.println("SensorMoisture: ");
+      Serial.println("*M"+String(sensorMoisture)+"*");
       BT1.println("*M"+String(sensorMoisture)+"*");
       digitalWrite(LED_BUILTIN, LOW);
       delay(1000);
+
+      // activar riego
+      if(sensorMoisture <= 10){ statusEV = 1; }
+      else { statusEV = 0; }
+      
+      Serial.println("statusEV: " + String(statusEV));
+      if(statusEV == 1){
+        Serial.println("EV Encendida: ");
+        digitalWrite(pinEV, LOW);
+      } else {
+        Serial.println("EV por defecto: ");
+        digitalWrite(pinEV, HIGH);
+      }
   }
 }
