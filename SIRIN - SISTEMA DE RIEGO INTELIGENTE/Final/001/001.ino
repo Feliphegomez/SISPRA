f6cf29b021f34608bfb29c527e2d487390971d36
Andres Felipe Gomez Maya <andres.gomez@MONTEVERDE.LOCAL>
2019-12-09 16:38:02 -0500

    Update.

--- SIRIN - SISTEMA DE RIEGO INTELIGENTE/Final/001/001.ino
+++ SIRIN - SISTEMA DE RIEGO INTELIGENTE/Final/001/001.ino
@@ -11,16 +11,18 @@ int pinDTH = 2;             // DHT  - PIN 2 para DTH
 int pinMoisture = A0;       // 
 int pinWaterSensor = A1;    // 
 
+int statusDevice = 0; //  StandBy, Checked
 volatile byte relayEVState = LOW;
+
+int last_cicle = 0;
+float temperature = 0;
+float humidity = 0;
 
 SimpleDHT22 dht22(pinDTH);
 SoftwareSerial BT1(pinRX, pinTX);  // Crear BT1 - pin 10 como RX, pin 11 como TX
 
-float temperature = 0;
-float humidity = 0;
-
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600);                 // comunicacion de monitor serial a 9600 bps
   while (!Serial) ; // wait for serial
   delay(200);
   pinMode(LED_BUILTIN, OUTPUT);
@@ -30,57 +32,44 @@ void setup() {
   pinMode(pinVCC_BT, OUTPUT);         // cuando se alimente de aqui
   // digitalWrite(pinForceAT, HIGH);   //  Forzar AT para configuracion BT
   delay(500);                         // Espera antes de encender el modulo
-  
   digitalWrite(pinVCC_BT, HIGH);     // Enciende el modulo BT
   BT1.begin(38400);                  // comunicacion serie entre Arduino y el modulo a 38400 bps
-  
   if(digitalRead(pinForceAT) == HIGH){
     Serial.println("Modo configuracion de AT Bluetooth:");
   }
-  
   pinMode(pinEV, OUTPUT);         // Electrovalvula
   digitalWrite(pinEV, HIGH);
-  // attachInterrupt(digitalPinToInterrupt(PIRInterrupt), detectMotion, RISING);
-}
-
-void loop() {
-  digitalWrite(LED_BUILTIN, LOW);
-  Serial.begin(9600);                 // comunicacion de monitor serial a 9600 bps
+  digitalWrite(LED_BUILTIN, HIGH);
+  
   if(digitalRead(pinForceAT) == HIGH){
     if (BT1.available()){ Serial.write(BT1.read()); }
     if (Serial.available()){ BT1.write(Serial.read()); }
   } else {
     BT1.listen();
   }
-  
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+  Serial.println("Ciclo Nuevo");
   int err = SimpleDHTErrSuccess;
   if ((err = dht22.read2(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
-    Serial.print("Read DHT22 failed, err="); Serial.println(err);
-    if (BT1.available()){  BT1.print("Read DHT22 failed, err="); BT1.println(err);  }
-    digitalWrite(LED_BUILTIN, HIGH); delay(250);
-    digitalWrite(LED_BUILTIN, LOW); delay(250);
-    digitalWrite(LED_BUILTIN, HIGH); delay(250);
-    digitalWrite(LED_BUILTIN, LOW); delay(250);
+    Serial.print("Read DHT22 failed, err=");
+    Serial.println(err);
+    if (BT1.available()){
+      BT1.print("Read DHT22 failed, err="); BT1.println(err);
+    }
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(250);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(250);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(250);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(250);
+    delay(1000);
     return;
   } else {
-      BT1.println("*T"+String(temperature)+"*");
-      BT1.println("*H"+String(humidity)+"*");
-
-      int sensorWater = map(analogRead(pinWaterSensor),0,300,0,100);
-      // Serial.println("*W"+String(sensorWater)+"*");
-      BT1.println("*W"+String(sensorWater)+"*");
-
-      int sensorMoisture =analogRead(pinMoisture);
-      sensorMoisture = map(sensorMoisture,1023,0,0,100);
-      // Serial.println("*M"+String(sensorMoisture)+"*");
-      BT1.println("*M"+String(sensorMoisture)+"*");
-      // digitalWrite(LED_BUILTIN, LOW);
-
-      Serial.print("T.A: " + String(temperature));
-      Serial.print(" | H.A: " + String(humidity));
-      Serial.print(" | W: " + String(sensorWater));
-      Serial.print(" | M: " + String(sensorMoisture));
-      Serial.println();
+     // Serial.println("Nuevo ciclo: " + String(millis()));
   }
-  delay(1000);
 }
